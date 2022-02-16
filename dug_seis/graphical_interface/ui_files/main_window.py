ac366c65f38fe8fce0c0090e6c5ac43e8b8dbaba
Lion Krischer <lion.krischer@gmail.com>
2022-02-16 15:00:49 +0100

    The waveforms now hide behind a scroll bar in case too many traces are shown.

--- dug_seis/graphical_interface/ui_files/main_window.py
+++ dug_seis/graphical_interface/ui_files/main_window.py
@@ -3,18 +3,27 @@
 ################################################################################
 ## Form generated from reading UI file 'main_window.ui'
 ##
-## Created by: Qt User Interface Compiler version 6.1.1
+## Created by: Qt User Interface Compiler version 6.2.3
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
-from PySide6.QtCore import *  # type: ignore
-from PySide6.QtGui import *  # type: ignore
-from PySide6.QtWidgets import *  # type: ignore
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QAbstractItemView, QApplication, QCheckBox, QComboBox,
+    QFrame, QGroupBox, QHBoxLayout, QLabel,
+    QListWidget, QListWidgetItem, QMainWindow, QMenuBar,
+    QPushButton, QScrollArea, QSizePolicy, QSpacerItem,
+    QSpinBox, QSplitter, QStatusBar, QTabWidget,
+    QTextBrowser, QVBoxLayout, QWidget)
 
 from pyqtgraph import GraphicsLayoutWidget
 from pyqtgraph.opengl import GLViewWidget
-
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -119,15 +128,28 @@ class Ui_MainWindow(object):
 
         self.verticalLayout_6.addLayout(self.horizontalLayout_7)
 
-        self.plotWidget = GraphicsLayoutWidget(self.groupBox)
+        self.scrollArea = QScrollArea(self.groupBox)
+        self.scrollArea.setObjectName(u"scrollArea")
+        self.scrollArea.setWidgetResizable(True)
+        self.scrollAreaWidgetContents = QWidget()
+        self.scrollAreaWidgetContents.setObjectName(u"scrollAreaWidgetContents")
+        self.scrollAreaWidgetContents.setGeometry(QRect(0, 0, 953, 375))
+        self.verticalLayout_9 = QVBoxLayout(self.scrollAreaWidgetContents)
+        self.verticalLayout_9.setObjectName(u"verticalLayout_9")
+        self.plotWidget = GraphicsLayoutWidget(self.scrollAreaWidgetContents)
         self.plotWidget.setObjectName(u"plotWidget")
         sizePolicy1 = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
         sizePolicy1.setHorizontalStretch(0)
         sizePolicy1.setVerticalStretch(0)
         sizePolicy1.setHeightForWidth(self.plotWidget.sizePolicy().hasHeightForWidth())
         self.plotWidget.setSizePolicy(sizePolicy1)
+        self.plotWidget.setMinimumSize(QSize(0, 0))
 
-        self.verticalLayout_6.addWidget(self.plotWidget)
+        self.verticalLayout_9.addWidget(self.plotWidget)
+
+        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
+
+        self.verticalLayout_6.addWidget(self.scrollArea)
 
         self.horizontalLayout_2 = QHBoxLayout()
         self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
