101ada2cb0f40064c1c0fb2cf51f6f9a35c5a3c6
memeier <menandrin.meier@erdw.ethz.ch>
2022-03-10 09:58:46 +0100

    small changes in scripts/mam/ (?)

--- scripts/mam/get_waveforms_DUGseis.py
+++ scripts/mam/get_waveforms_DUGseis.py
@@ -5,12 +5,20 @@ from obspy.core import UTCDateTime
 #folder = "/home/user/mrosskopf/Projects/CODES/"
 #project = DUGSeisProject(config=folder + "DUGseis_example.yaml")
 project = DUGSeisProject(config="/Users/mameier/programs/seismo/projects/bedretto/DUGseis/scripts/mam/DUGseis_example.yaml")
+database = project.db
 
-interval_start = UTCDateTime(2022,2,3,21,30)
-interval_end = UTCDateTime(2022,2,3,22)
+
+interval_start = UTCDateTime('2022-02-03T21:30:00')
+interval_end = UTCDateTime('2022-02-03T21:30:05')
+
+#channel_ids=["XB.02.01.001"]
+channel_ids='all_channels'
+
+print(interval_end)
 
 st_event = project.waveforms.get_waveforms(
-            channel_ids=all_channels,
+            channel_ids=["XB.02.03.001","XB.03.01.001",],
             start_time=interval_start,
             end_time=interval_end,
         )
+print('coucou!')
---
+++ scripts/mam/read_databaseRoutine.py
@@ -0,0 +1,48 @@
+from dug_seis.project.project import DUGSeisProject
+
+folder = "/home/user/VALTER_EXPERIMENT/INTERVAL7/CODES/"
+project = DUGSeisProject(config=folder + "post_processing_VALTERStimu1_smallWin.yaml")
+
+
+##########################
+# get information from event and channels with picks
+##########################
+
+# read project database
+database = project.db
+# get events from the database
+if True:
+    events = database.get_objects(object_type="Event")
+
+    origin_time = []
+    pick_ch = []
+    ev_lat = []
+    ev_lon = []
+    ev_depth = []
+
+    # get picks, location and origin time from events -> structure like obspy events
+
+    for ev in events:
+        evpick = []
+        ch = []
+        for pick in ev.picks:
+            utc = ev.origins[0].time
+            evpick.append(utc.datetime)
+            ch.append(pick.waveform_id.station_code)
+
+        origin_time.append(evpick)
+        pick_ch.append(ch)
+        ev_lat.append(ev.origins[0].latitude)
+        ev_lon.append(ev.origins[0].longitude)
+        ev_depth.append(ev.origins[0].depth)
+
+# get unassociated events from the database
+unas_picks = []
+unas_picks_ch = []
+unas_picks_phase
+u_picks = database.get_unassociated_picks()
+for u_pick in u_picks:
+    utc = u_pick.time
+    unas_picks.append(utc.datetime)
+    unas_picks_ch.append(u_pick.waveform_id.station_code) #or network_code, channel_code, location_code
+    unas_picks_phase.append(u_pick.phase_hint)
